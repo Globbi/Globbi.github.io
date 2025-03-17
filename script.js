9a1cd5f02244fd9a6d6fba2779534e7f813e7bc4
Globbi <globbi@globington.local>
2025-03-17 16:12:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,115 @@
+// seeded random number generator
+function splitmix32(a) {
+    return function() {
+        a |= 0;
+        a = a + 0x9e3779b9 | 0;
+        let t = a ^ a >>> 16;
+        t = Math.imul(t, 0x21f0aaad);
+        t = t ^ t >>> 15;
+        t = Math.imul(t, 0x735a2d97);
+        return ((t = t ^ t >>> 15) >>> 0) / 4294967296;
+    }
+}
+
+// parses the query parameters
+function parseConfig() {
+    const config = { seed: null, assignment: null };
+    const query = window.location.search.substring(1);
+    const vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        const pair = vars[i].split("=");
+        const key = decodeURIComponent(pair[0]);
+        switch (key) {
+            case "assignment":
+                config.assignment = decodeURIComponent(pair[1]);
+                break;
+            case "seed":
+                const seed = Number(decodeURIComponent(pair[1]));
+                if (!isNaN(seed)) {
+                    config.seed = seed;
+                }
+                break;
+            default:
+                console.warn("unknown query parameter key '%s'.", key)
+                break;
+        }
+    }
+    return config;
+}
+
+// create a seed
+function createSeed() {
+    return Math.floor(Math.random() * 10000);
+}
+
+// predefined set of assignments
+const assignments = {
+    linear_null: {
+        title: "Bestimme die Nullpunkte der linearen Funktionen (x- und y-Achse)",
+        generator: function(prng) {
+            const signA = prng() > 0.5 ? 1 : -1;
+            const a = Math.floor(prng()*9) + 1;
+
+            signB = prng() > 0.5 ? "+" : "-";
+            const b = Math.floor(prng()*10);
+            return `\\(f(x) = ${signA * a}x ${signB} ${b}\\)`
+        }
+    }
+};
+
+function main() {
+    const config = parseConfig();
+
+    if (config.seed === null) {
+        // Seed missing -> create new
+        config.seed = createSeed();
+        const url = new URL(location);
+        url.searchParams.set("seed", config.seed);
+        window.history.replaceState({}, "", url);
+    }
+
+    // set seed for display
+    const titleText = document.querySelector("#seed");
+    titleText.innerHTML = "#" + config.seed;
+    titleText.addEventListener("click", () => {
+        if (confirm("Neuen Seed generieren?")) {
+            const url = new URL(location);
+            url.searchParams.delete("seed");
+            window.location.href = url;
+        }
+    });
+
+
+    const assignment = assignments[config.assignment];
+
+    // set assignment title
+    if (!!assignment) {
+        const titleText = document.querySelector("#title-text");
+        titleText.innerHTML = assignment.title;
+    }
+
+    // generate assignments
+    const prng = splitmix32(config.seed);
+    const generator = assignment.generator;
+
+    const tasks = document.querySelector(".tasks");
+    const codePointA = "a".codePointAt(0);
+    const taskTemplate = document.querySelector("#task-template");
+    for (let i = 0; i < 12; i++) {
+        const task = taskTemplate.content.cloneNode(true);
+        // set label
+        const label = task.querySelector(".label");
+        label.innerHTML = String.fromCodePoint(codePointA + i) + ")";
+        // set math
+        const math = task.querySelector(".math");
+        math.innerHTML = generator(prng);
+        // append child
+        tasks.appendChild(task);
+    }
+
+    if (!!window.MathJax) {
+        MathJax.typeset();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", main, false);
